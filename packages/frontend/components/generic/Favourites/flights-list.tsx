09188b13b91436dcc2e852a3ed0c15bac56d7858
joanoliete <joliete@edu.tecnocampus.cat>
2021-05-31 17:33:39 +0200

    User friendly messages and welcome message on startup view

--- packages/frontend/components/generic/Favourites/flights-list.tsx
+++ packages/frontend/components/generic/Favourites/flights-list.tsx
@@ -4,16 +4,21 @@ import ReactPaginate from 'react-paginate';
 import Router from 'next/router';
 import { useRouter } from 'next/router';
 import FlightCard from './flight-card';
+import { useSession } from 'next-auth/client';
 
 type FlightsListProps = {
 	list: any | null;
 };
 
 const FlightsList: FC<FlightsListProps> = ({ list }) => {
+	const [session, loadingSession] = useSession();
 	return (
 		<div className='bg-white pb-4 px-4 rounded-md w-full flex-grow'>
 			<div className='items-center flex flex-col justify-center'>
-				<h2 className='text-2xl inset-0 pb-6 p-4'>Your favourite flights</h2>
+				<h2 className='text-2xl inset-0 pb-2 p-4'>Your favourite flights</h2>
+				<p className='pb-4'>
+					Hello <b>{session.user.email}</b>!
+				</p>
 				<ul className='space-y-2'>
 					{list.length == 0 && (
 						<p>
--- packages/frontend/components/searchForm/SearchForm.tsx
+++ packages/frontend/components/searchForm/SearchForm.tsx
@@ -9,16 +9,25 @@ import Router from 'next/router';
 import { useRouter } from 'next/router';
 import PlacePickers from './PlacePickers';
 import DatePickers from './DatePickers';
+import { getSession, useSession } from 'next-auth/client';
 import SearchMode from './SearchMode';
 import { SearchIcon } from '../icons/header/search-icon';
 
 const SearchForm: FC<any> = ({ show }) => {
+	const [session, loadingSession] = useSession();
 	const onClick = () => show(true);
 
 	return (
 		<div className='bg-white px-4 w-full'>
 			<div className='items-center flex flex-col justify-center'>
-				<h2 className='text-2xl inset-0 pb-6 p-4'>Search flights</h2>
+				<h2 className='text-2xl inset-0 pb-2 p-4'>Search flights</h2>
+				{!loadingSession && !session ? (
+					<>
+						<p className='pb-4'>
+							Login to track favourite flights and automatize search
+						</p>
+					</>
+				) : null}
 				<div className='text-sm font-normal text-gray-700 border rounded-lg border-b-0 shadow-lg items-center flex flex-col justify-center pb-6 px-2'>
 					<SearchMode />
 					<div className='flex flex-wrap justify-center pb-2 sm:pt-0 pt-3'>
