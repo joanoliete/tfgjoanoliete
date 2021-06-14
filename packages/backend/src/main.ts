119ba58d9fcaa4ec0f525d93e7e805d218a9a781
joanoliete <joliete@edu.tecnocampus.cat>
2021-06-14 19:58:32 +0200

    Production changes

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
